d1c32152f8006301ce3c1f23184e109f71959440
GimJunGyeom <highjun90@gmail.com>
2023-09-12 15:06:17 +0900

    불필요 파일 제거

--- DirectX2D_MegamanX5/GameEngineContents/ContentsCore.cpp
+++ DirectX2D_MegamanX5/GameEngineContents/ContentsCore.cpp
@@ -1,7 +1,5 @@
 #include "PreCompile.h"
 #include "ContentsCore.h"
-#include "PlayLevel.h"
-#include "TitleLevel.h"
 #include "LevelTitle.h"
 #include "LevelcharacterSelect.h"
 #include "LevelJungle.h"
@@ -19,9 +17,7 @@ void ContentsCore::Start()
 	//기본적으로 SpriteRenderer를 만들때 넣어줄 샘플러를 지정합니다. 이 코드 없으면 static 초기화 에러가 날것임
 	GameEngineSpriteRenderer::SetDefaultSampler("POINT");
 
-	GameEngineCore::CreateLevel<PlayLevel>("PlayLevel");
-	GameEngineCore::CreateLevel<TitleLevel>("TitleLevel");
-	//GameEngineCore::ChangeLevel("PlayLevel");
+
 
 	GameEngineCore::CreateLevel<LevelTitle>("LevelTitle");
 	GameEngineCore::CreateLevel<LevelCharacterSelect>("LevelCharacterSelect");
