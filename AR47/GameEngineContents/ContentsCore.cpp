9f054a3fe6980406f204ba05e00ebc4022ef12cb
highjun90@gmail.com <highjun90@gmail.com>
2023-09-19 12:42:23 +0900

    lib 파일 검사

--- AR47/GameEngineContents/ContentsCore.cpp
+++ AR47/GameEngineContents/ContentsCore.cpp
@@ -1,29 +1,53 @@
+//#include "PreCompile.h"
+//#include "ContentsCore.h"
+//
+//#include <GameEngineCore/GameEngineSampler.h>
+//#include <GameEngineCore/GameEngineSpriteRenderer.h>
+//
+//ContentsCore::ContentsCore()
+//{
+//}
+//
+//ContentsCore::~ContentsCore()
+//{
+//}
+//
+//void ContentsCore::Start()
+//{
+//
+//
+//}
+//
+//void ContentsCore::Update(float _Delta)
+//{
+//
+//}
+//
+//void ContentsCore::Release()
+//{
+//
+//}
+
 #include "PreCompile.h"
 #include "ContentsCore.h"
+#include "LevelTitle.h"
+#include "LevelcharacterSelect.h"
+#include "LevelJungle.h"
 
-#include <GameEngineCore/GameEngineSampler.h>
-#include <GameEngineCore/GameEngineSpriteRenderer.h>
-
-#include "PlayLevel.h"
-#include "TitleLevel.h"
-
-ContentsCore::ContentsCore() 
+ContentsCore::ContentsCore()
 {
 }
 
-ContentsCore::~ContentsCore() 
+ContentsCore::~ContentsCore()
 {
 }
 
 void ContentsCore::Start()
 {
-	//기본적으로 SpriteRenderer를 만들때 넣어줄 샘플러를 지정합니다.
-	GameEngineCore::CreateLevel<PlayLevel>("PlayLevel");
-	GameEngineCore::CreateLevel<TitleLevel>("TitleLevel");
-	GameEngineCore::ChangeLevel("PlayLevel");
-
-	// 자기 텍스처 로드해야 한다.
-
+	GameEngineCore::CreateLevel<LevelTitle>("LevelTitle");
+	GameEngineCore::CreateLevel<LevelCharacterSelect>("LevelCharacterSelect");
+	GameEngineCore::CreateLevel<LevelJungle>("LevelJungle");
+	GameEngineCore::ChangeLevel("LevelTitle");
 }
 
 void ContentsCore::Update(float _Delta)
--- AR47/GameEngineContents/ContentsCore.h
+++ AR47/GameEngineContents/ContentsCore.h
@@ -1,3 +1,46 @@
+//#pragma once
+//#include <GameEngineCore/GameEngineObject.h>
+//#include <GameEngineCore/GameEngineCore.h>
+//
+//// Ό³Έν :
+//class ContentsCore : public GameEngineObject
+//{
+//public:
+//	static std::string GetWindowTitle()
+//	{
+//		return "MegamanX5";
+//	}
+//
+//	static float4 GetStartWindowSize()
+//	{
+//		return { 1280, 720 };
+//	}
+//
+//	static float4 GetStartWindowPos()
+//	{
+//		return { 200, 200 };
+//	}
+//
+//	// constrcuter destructer
+//	ContentsCore();
+//	~ContentsCore();
+//
+//	// delete Function
+//	ContentsCore(const ContentsCore& _Other) = delete;
+//	ContentsCore(ContentsCore&& _Other) noexcept = delete;
+//	ContentsCore& operator=(const ContentsCore& _Other) = delete;
+//	ContentsCore& operator=(ContentsCore&& _Other) noexcept = delete;
+//
+//protected:
+//	void Start() override;
+//	void Update(float _Delta) override;
+//	void Release() override;
+//
+//
+//private:
+//
+//};
+
 #pragma once
 #include <GameEngineCore/GameEngineObject.h>
 #include <GameEngineCore/GameEngineCore.h>
@@ -8,17 +51,17 @@ class ContentsCore : public GameEngineObject
 public:
 	static std::string GetWindowTitle()
 	{
-		return "MainWindow";
+		return "Megaman X5";
 	}
 
 	static float4 GetStartWindowSize()
 	{
-		return {1280, 720};
+		return { 960, 720 };
 	}
 
 	static float4 GetStartWindowPos()
 	{
-		return { 200, 200 };
+		return { 0, 0 };
 	}
 
 	// constrcuter destructer
@@ -40,4 +83,6 @@ protected:
 private:
 
 };
+
+
 
